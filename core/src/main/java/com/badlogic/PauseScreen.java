753af00dfcb989d2bdb5e640d128843107a04553
nujra-panda <160462841+nujra-panda@users.noreply.github.com>
2024-11-29 23:08:07 +0530

    Update PauseScreen.java

--- core/src/main/java/com/badlogic/PauseScreen.java
+++ core/src/main/java/com/badlogic/PauseScreen.java
@@ -59,6 +59,7 @@ public class PauseScreen implements Screen {
             float delta = Gdx.graphics.getDeltaTime();
             game.addTimer(delta);
 
+            System.out.println(game.getTimer());
             if (game.getTimer() > 0.1f) {
                 if (resume.contains(touchPos.x, touchPos.y)) {
                     game.setTimer(0);
